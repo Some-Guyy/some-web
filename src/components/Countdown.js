7492878326f4b34d744e9ae5b6d7eac5a7ceaa88
Some Guy <ozruen@gmail.com>
2019-09-06 10:08:08 +0800

    0.3.4 - Split numbers by commas in Countdown

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -3,38 +3,36 @@ import React, { Component } from 'react';
 class Countdown extends Component {
     state = {
         end_date: new Date("Feb 14, 2020 17:30:00").getTime(),
-        now: 'none',
-        distance: 'none',
-        days: 'none',
-        hours: 'none',
-        minutes: 'none',
-        seconds: 'none'
+        now: NaN,
+        distance: NaN,
+        days: NaN,
+        hours: NaN,
+        minutes: NaN,
+        seconds: NaN
     }
+
+    commafyNumber = (number) => number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+
     timer = () => {
         this.setState({ now: new Date().getTime() })
         this.setState({
             distance: this.state.end_date - this.state.now,
-            days: Math.floor(this.state.distance / (1000 * 60 * 60 * 24)),
-            hours: Math.floor(this.state.distance / (1000 * 60 * 60)),
-            minutes: Math.floor(this.state.distance / (1000 * 60)),
-            seconds: Math.floor(this.state.distance / 1000)
+            days: this.commafyNumber(Math.floor(this.state.distance / (1000 * 60 * 60 * 24))),
+            hours: this.commafyNumber(Math.floor(this.state.distance / (1000 * 60 * 60))),
+            minutes: this.commafyNumber(Math.floor(this.state.distance / (1000 * 60))),
+            seconds: this.commafyNumber(Math.floor(this.state.distance / 1000))
         })
         if (this.state.distance < 0) {
             clearInterval(this.intervalId);
         }
     }
+
     componentDidMount = () => {
         this.setState({ now: this.state.end_date - new Date().getTime() })
         this.setState({ distance: this.state.end_date - this.state.now })
-        this.setState({
-            days: Math.floor(this.state.distance / (1000 * 60 * 60 * 24)),
-            hours: Math.floor(this.state.distance / (1000 * 60 * 60)),
-            minutes: Math.floor(this.state.distance / (1000 * 60)),
-            seconds: Math.floor(this.state.distance / 1000)
-        })
         this.intervalId = setInterval(this.timer, 1000);
     }
-    componentWillUnmount = () => { clearInterval(this.intervalId); }
+    componentWillUnmount = () => clearInterval(this.intervalId)
 
     render() {
         const { days, hours, minutes, seconds } = this.state;
