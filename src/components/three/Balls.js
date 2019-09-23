9751e5e4a49d868f32122930be8e3a6ad5a5979d
Some Guy <ozruen@gmail.com>
2019-09-23 13:54:18 +0800

    0.5.4 - Changed spin to a random value

---
+++ src/components/three/Balls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import * as THREE from 'three';
+import { TimelineMax, Expo } from 'gsap';
+
+import '../../App.css';
+
+export default function Balls() {
+    return (
+        <React.Fragment></React.Fragment>
+    );
+}
--- src/components/three/Cubes.js
+++ src/components/three/Cubes.js
@@ -11,26 +11,24 @@ export default function Cubes() {
     );
 }
 
+// Setting up cameras and renderers
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
-
 let renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#222");
 renderer.setSize(window.innerWidth, window.innerHeight - 80);
-
 document.body.appendChild(renderer.domElement);
 window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight - 80);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 });
-
 let raycaster = new THREE.Raycaster();
 let mouse = new THREE.Vector2();
 
+// Setting up objects
 let meshArray = [];
-
 for (let i = 0; i < 20; i++) {
     let geometry = new THREE.BoxGeometry(Math.random() * 3, Math.random() * 3, Math.random() * 3);
     let material = new THREE.MeshLambertMaterial({ color: Math.random() * 0xFFFFFF });
@@ -41,12 +39,11 @@ for (let i = 0; i < 20; i++) {
     scene.add(mesh);
     meshArray.push(mesh);
 }
-
 let light = new THREE.PointLight(0xFFFFFF, 1, 500);
 light.position.set(10, 0, 25);
 scene.add(light);
 
-let render = () => {
+const render = () => {
     requestAnimationFrame(render);
 
     for (var meshObject in meshArray) {
@@ -68,7 +65,7 @@ const onHover = event => {
     let intersects = raycaster.intersectObjects(scene.children, true);
     for (let i = 0; i < intersects.length; i++) {
         let animationSpin = new TimelineMax();
-        animationSpin.to(intersects[i].object.rotation, 3, { y: 15, ease: Expo.easeOut });
+        animationSpin.to(intersects[i].object.rotation, 3, { y: Math.random() * 13, ease: Expo.easeOut });
     }
 }
 
