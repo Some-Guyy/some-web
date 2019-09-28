8d82f59e164142f9929183298afc248b350f32b3
Some Guy <somenormaladdress@gmail.com>
2019-09-29 00:36:01 +0800

    Removed threeJS cubes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Countdown from './countdown/Countdown';
-import Cubes from './three/Cubes';
 
 export default class Home extends Component {
   render() {
     return (
       <div className="onCanvas">
         <Countdown />
-        <Cubes />
       </div>
     );
   }
