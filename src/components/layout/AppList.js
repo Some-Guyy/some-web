a13cdad8f853990571788c90cf50d0626120cd39
Some <somenormaladdress@gmail.com>
2020-02-07 15:45:55 +0800

    complete link to countdown

--- src/components/layout/AppList.js
+++ src/components/layout/AppList.js
@@ -5,7 +5,7 @@ import '../../App.css';
 export default function AppList(props) {
     return (
         <div style={{ textAlign: 'center' }}>
-            <p className="textLink" style={{ fontSize: '20px' }}></p>
+            <p className="textLink" onClick={_ => props.appSelect('countdown')} style={{ fontSize: '20px' }}>Click here for the countdown app!</p>
         </div>
         )
     }
