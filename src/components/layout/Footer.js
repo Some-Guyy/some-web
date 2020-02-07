0acc6b4fed27dfabad331eb52e4d3e88be7d74ea
Some <somenormaladdress@gmail.com>
2020-02-07 13:22:32 +0800

    Updated year

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -6,7 +6,7 @@ const pckg = require('../../../package.json')
 export default function Footer() {
     return (
         <footer className="colorBackDominant">
-            <span style={{ float: "left", marginLeft: '5px' }}>Ⓢ 2019 - Some Guy. Some Rights Reserved.</span>
+            <span style={{ float: "left", marginLeft: '5px' }}>Ⓢ 2020 - Some Guy. Some Rights Reserved.</span>
             <span style={{ float: "right", marginRight: '5px' }}>v{pckg.version}</span>
         </footer>
     )
