dd795a559c3a9673bec3c95716705e580c457337
Some <somenormaladdress@gmail.com>
2020-02-07 13:22:18 +0800

    Changed more paths and updated packages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 import CookieNotice from './components/utilities/CookieNotice';
+import Header from './components/layout/Header';
 import Body from './components/layout/Body';
+import Footer from './components/layout/Footer';
 
 import './App.css';
 
@@ -10,7 +12,9 @@ export default class App extends Component {
     return (
       <div>
         <CookieNotice />
+        <Header />
         <Body />
+        <Footer />
       </div>
     );
   }
--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import Header from './Header';
-import Footer from './Footer';
 import Countdown from '../countdown/Countdown';
 
 import '../../App.css';
@@ -10,9 +8,7 @@ export default class Body extends Component {
   render() {
     return (
       <div>
-        <Header />
         <Countdown />
-        <Footer />
       </div>
     );
   }
