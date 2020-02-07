aed46b9b751988c35f265a261eb83d9c33da77e4
Some <somenormaladdress@gmail.com>
2020-02-07 13:51:10 +0800

    created applist component

---
+++ src/components/layout/AppList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import '../../App.css';
+
+export default function AppList () {
+    return (
+        <p>list of all the apps</p>
+    )
+}
--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 
+import AppList from './AppList';
 import Countdown from '../countdown/Countdown';
 
 import '../../App.css';
 
 export default function Body() {
-  const [appView, setAppView] = useState('');
+  const [appView, setAppView] = useState('home');
   return (
     <React.Fragment>
-      {appView === ''
-        ? <Countdown />
-        : <p>hi</p>}
+      {
+        appView === 'home'
+          ? <AppList />
+          : <Countdown />
+      }
     </React.Fragment>
   )
 }
