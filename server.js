4a519f6e09b40d65324059675ca86f86c34d66c0
Some <somenormaladdress@gmail.com>
2020-02-10 14:34:32 +0800

    Use server instead of app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const server = require("http").Server(app);
 const port = 8080;
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -9,4 +10,4 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port, _ => console.log(`Some website listening on port ${port}!`));
+server.listen(port, _ => console.log(`Some website listening on port ${port}!`));
