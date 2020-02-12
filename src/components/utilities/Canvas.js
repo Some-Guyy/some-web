383417166e69bf50bd54a726ecde2431be3c3dc4
Some <somenormaladdress@gmail.com>
2020-02-12 17:37:47 +0800

    Moved sketch into function

--- src/components/utilities/Canvas.js
+++ src/components/utilities/Canvas.js
@@ -1,51 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import io from 'socket.io-client';
 
 import '../../App.css';
 
 const socket = io.connect();
-const brushSize = 20;
 
 export default function Canvas() {
-    return (<P5Wrapper sketch={sketch}></P5Wrapper>)
-}
+    const [brushSize, setBrushSize] = useState(20);
+    const sketch = p => {
+        // When canvas launches.
+        p.setup = _ => {
+            socket.emit('requestCanvasState', '');
+            p.createCanvas(1000, 800);
+            p.background(255);
+        }
 
-const sketch = p => {
-    // When canvas launches.
-    p.setup = _ => {
-        socket.emit('requestCanvasState', '');
-        p.createCanvas(1000, 800);
-        p.background(255);
-    }
+        // When drawing.
+        p.mouseDragged = _ => {
+            // Send drawing data to the server.
+            const data = {
+                drawX: p.mouseX,
+                drawY: p.mouseY,
+                brushSize
+            };
+            socket.emit('clientDraw', data);
 
-    // When drawing.
-    p.mouseDragged = _ => {
-        // Send drawing data to the server.
-        const data = {
-            drawX: p.mouseX,
-            drawY: p.mouseY,
-            brushSize
-        };
-        socket.emit('clientDraw', data);
-
-        p.noStroke();
-        p.fill(0);
-        p.ellipse(p.mouseX, p.mouseY, brushSize);
-        console.log(`${data['drawX']}, ${data['drawY']}`);
-    }
-
-    socket.on('canvasState', data => {
-        for (let i = 0; i < data.length; i++) {
             p.noStroke();
             p.fill(0);
-            p.ellipse(data[i].drawX, data[i].drawY, data[i].brushSize);
+            p.ellipse(p.mouseX, p.mouseY, brushSize);
+            console.log(`${data['drawX']}, ${data['drawY']}`);
         }
-    });
 
-    socket.on('serverDraw', data => {
-        p.noStroke();
-        p.fill(0);
-        p.ellipse(data.drawX, data.drawY, data.brushSize);
-    });
+        socket.on('canvasState', data => {
+            for (let i = 0; i < data.length; i++) {
+                p.noStroke();
+                p.fill(0);
+                p.ellipse(data[i].drawX, data[i].drawY, data[i].brushSize);
+            }
+        });
+
+        socket.on('serverDraw', data => {
+            p.noStroke();
+            p.fill(0);
+            p.ellipse(data.drawX, data.drawY, data.brushSize);
+        });
+    }
+
+    return (<P5Wrapper sketch={sketch}></P5Wrapper>)
 }
