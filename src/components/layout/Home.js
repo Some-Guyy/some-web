ea7ee70a20ebeaa2ee07df83d18c9aa200395710
Some <somenormaladdress@gmail.com>
2020-02-13 17:51:20 +0800

    Changed Homepage format, added icons

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
-import Canvas from '../utilities/Canvas';
-
 import '../../App.css';
 
+import Canvas from '../utilities/Canvas';
+import clockIcon from '../../assets/icons/icons8-clock-128.png';
+
 export default function AppList(props) {
+    const apps = [{ name: 'Countdown', codeName: 'countdown', iconPath: clockIcon }];
     return (
         <div style={{ textAlign: 'center', padding: '10px' }}>
             <p style={{ fontSize: '24px' }}>Hi, this is just some website.<br />While you're here, go ahead and leave your mark below!</p>
             <Canvas />
-            <p className="textLink" onClick={_ => props.appSelect('countdown')} style={{ fontSize: '20px' }}>Click here for the countdown app!</p>
+            <p style={{ fontSize: '24px', textDecoration: 'underline' }}>Check out some other apps we have.</p>
+            {apps.map(app => <div className="app textLinkLight" onClick={_ => props.appSelect(app.codeName)}><img src={app.iconPath} alt="icon" /><p style={{ fontSize: '18px' }}>{app.name}</p></div>)}
         </div>
     )
 }
