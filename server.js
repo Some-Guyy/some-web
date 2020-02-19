c8717387a3022ea4fd94e34ad4f53b0ac7e1a93e
Some <somenormaladdress@gmail.com>
2020-02-19 14:23:33 +0800

    Improved logging process

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 const express = require('express');
 const app = express();
 const port = 8080;
 
+const logPath = path.join(__dirname, 'logs', 'console.log');
 const canvasPath = path.join(__dirname, 'logs', 'canvas.json');
+
+// Save console logs.
+const logFile = fs.createWriteStream(logPath, {flags: 'a'});
+const logStdout = process.stdout;
+console.log = log => {
+  logFile.write(`${util.format(log)}\n`);
+  logStdout.write(`${util.format(log)}\n`);
+}
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -13,21 +23,20 @@ app.get('/*', function (req, res) {
 });
 
 // Websocket server stuff.
-const server = app.listen(port, _ => console.log(`Some website listening on port ${port}!\n`));
+const server = app.listen(port, _ => console.log(`Server startup success!\nTime: ${new Date().toString()}\nPort: ${port}\n`));
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
-  const logConnect = `New Connection!\nTime: ${new Date().toString()}\nID: ${socket.id}\nIP: ${socket.handshake.headers['x-forwarded-for']}\n`;
-  console.log(logConnect);
+  console.log(`New Connection!\nTime: ${new Date().toString()}\nIP: ${socket.handshake.headers['x-forwarded-for']}\nID: ${socket.id}\n`);
   socket.on('requestCanvasState', _ => socket.emit('canvasState', canvasState));
 
   socket.on('clientDraw', data => {
-    data.ID = socket.id;
     if (socket.handshake.headers['x-forwarded-for'] === undefined) {
       data.IP = 'undefined'
     } else {
       data.IP = socket.handshake.headers['x-forwarded-for'];
     }
+    data.ID = socket.id;
     socket.broadcast.emit('serverDraw', data);
     canvasState.push(data);
   });
@@ -40,5 +49,5 @@ if (fs.existsSync(canvasPath)) {
   var canvasState = JSON.parse('[]'); // Create a new canvasState if it doesn't exist.
 }
 
-// Save canvas state
+// Save canvas state periodically.
 setInterval(_ => fs.writeFileSync(canvasPath, JSON.stringify(canvasState)), 1000);
