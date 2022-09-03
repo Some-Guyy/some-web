f8eda116b03ed20b550210751124c19c13ad15d2
some guy <ozruen@gmail.com>
2022-09-04 05:27:14 +0800

    upgraded license component to include publisher and url if possible

--- src/components/Licenses.js
+++ src/components/Licenses.js
@@ -6,20 +6,23 @@ import licenseData from '../assets/data/licenses.json';
 const Licenses = ({ brandColorStrict }) => {
     // Place licenses that aren't in node modules into this array manually.
     const licenseArray = [
-        {
-            name: 'Box Icons',
-            repository: 'https://github.com/atisawd/boxicons',
-            license: `License
-            The MIT License (MIT)
-    
-            Copyright (c) 2015-2021 Aniket Suvarna
-    
-            Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-    
-            The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-    
-            THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
-        }
+        [
+            'Box Icons',
+            {
+                repository: 'https://github.com/atisawd/boxicons',
+                publisher: 'Aniket Suvarna',
+                url: 'https://boxicons.com/',
+                licenseText: `The MIT License (MIT)
+
+                Copyright (c) 2015-2021 Aniket Suvarna
+                
+                Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                
+                The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                
+                THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+            }
+        ]
     ]
     // Convert JSON data into array to map into the page.
     const npmLicenseArray = Object.keys(licenseData).map(key => [key, licenseData[key]]);
@@ -29,17 +32,35 @@ const Licenses = ({ brandColorStrict }) => {
             {
                 licenseArray.map(library => (
                     <React.Fragment key={uuidv4()}>
-                        <Row key={uuidv4()}><Link key={uuidv4()} color={brandColorStrict} href={library['repository']} target='_blank'>{library['name']}</Link></Row>
-                        <Row key={uuidv4()}><Text key={uuidv4()}>{library['license']}</Text></Row>
+                        <Row key={uuidv4()}>
+                            <Link key={uuidv4()} color={brandColorStrict} href={library[1]['repository']} target='_blank'>{library[0]}</Link>
+                            {
+                                library[1]['publisher'] && library[1]['url'] ?
+                                    <><Spacer key={uuidv4()} x={0.25} /><Text>by</Text><Spacer key={uuidv4()} x={0.25} /><Link key={uuidv4()} color={brandColorStrict} href={library[1]['url']} target='_blank'>{library[1]['publisher']}</Link></>
+                                    : library[1]['publisher'] ?
+                                        <><Spacer key={uuidv4()} x={0.25} /><Text>by</Text><Spacer key={uuidv4()} x={0.25} /><Text key={uuidv4()}>{library[1]['publisher']}</Text></>
+                                        : <></>
+                            }
+                        </Row>
+                        <Row key={uuidv4()}><Text key={uuidv4()}>{library[1]['licenseText']}</Text></Row>
                         <Spacer key={uuidv4()} y={0.5} />
                     </React.Fragment>
                 ))
             }
             {
-                npmLicenseArray.map(item => (
+                npmLicenseArray.map(library => (
                     <React.Fragment key={uuidv4()}>
-                        <Row key={uuidv4()}><Link key={uuidv4()} color={brandColorStrict} href={item[1]['repository']} target='_blank'>{item[0]}</Link></Row>
-                        <Row key={uuidv4()}><Text key={uuidv4()}>{item[1]['licenseText']}</Text></Row>
+                        <Row key={uuidv4()}>
+                            <Link key={uuidv4()} color={brandColorStrict} href={library[1]['repository']} target='_blank'>{library[0]}</Link>
+                            {
+                                library[1]['publisher'] && library[1]['url'] ?
+                                    <><Spacer key={uuidv4()} x={0.25} /><Text>by</Text><Spacer key={uuidv4()} x={0.25} /><Link key={uuidv4()} color={brandColorStrict} href={library[1]['url']} target='_blank'>{library[1]['publisher']}</Link></>
+                                    : library[1]['publisher'] ?
+                                        <><Spacer key={uuidv4()} x={0.25} /><Text>by</Text><Spacer key={uuidv4()} x={0.25} /><Text key={uuidv4()}>{library[1]['publisher']}</Text></>
+                                        : <></>
+                            }
+                        </Row>
+                        <Row key={uuidv4()}><Text key={uuidv4()}>{library[1]['licenseText']}</Text></Row>
                         <Spacer key={uuidv4()} y={0.5} />
                     </React.Fragment>
                 ))
