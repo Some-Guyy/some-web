1166af3b4e0f0d4da820b6a6f94dc07d20bf9caf
some guy <ozruen@gmail.com>
2022-09-17 00:41:10 +0800

    refactor code for acknowledgement

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,20 @@
 import { Container, Row, Text, Spacer, Link, Collapse } from '@nextui-org/react';
+import { v4 as uuidv4 } from 'uuid';
 import { HeartIconDark, HeartIconLight, CertificationIconDark, CertificationIconLight } from './Icons';
 import Licenses from './Licenses';
 import pckg from '../../package.json';
 
 const About = ({ darkMode, brandColor, brandColorStrict }) => {
+    // Place main installed packages and other acknowledgements into this array manually.
+    const acknowledgeArray = [
+        ["React", "https://reactjs.org/"],
+        ["NextUI", "https://nextui.org/"],
+        ["@fisch0920/use-dark-mode", "https://github.com/transitive-bullshit/use-dark-mode"],
+        ["uuid", "https://github.com/uuidjs/uuid"],
+        ["Boxicons", "https://boxicons.com/"],
+        ["Express", "https://expressjs.com/"]
+    ]
+
     return (
         <Container fluid>
             <Row justify='center'>
@@ -19,24 +30,13 @@ const About = ({ darkMode, brandColor, brandColorStrict }) => {
             <Container sm>
                 <Collapse.Group shadow accordion={false}>
                     <Collapse title='Acknowledgments' subtitle='This website was made with the help of these open source libraries' contentLeft={darkMode.value ? <HeartIconDark /> : <HeartIconLight />}>
-                        <Link color={brandColorStrict} href='https://reactjs.org/' target='_blank'>
-                            React
-                        </Link>
-                        <Link color={brandColorStrict} href='https://nextui.org/' target='_blank'>
-                            NextUI
-                        </Link>
-                        <Link color={brandColorStrict} href='https://github.com/transitive-bullshit/use-dark-mode' target='_blank'>
-                            @fisch0920/use-dark-mode
-                        </Link>
-                        <Link color={brandColorStrict} href='https://github.com/uuidjs/uuid' target='_blank'>
-                            uuid
-                        </Link>
-                        <Link color={brandColorStrict} href='https://boxicons.com/' target='_blank'>
-                            Boxicons
-                        </Link>
-                        <Link color={brandColorStrict} href='https://expressjs.com/' target='_blank'>
-                            Express
-                        </Link>
+                        {
+                            acknowledgeArray.map(acknowledgement => (
+                                <Link key={uuidv4()} color={brandColorStrict} href={acknowledgement[1]} target='_blank'>
+                                    {acknowledgement[0]}
+                                </Link>
+                            ))
+                        }
                     </Collapse>
                     <Collapse title='Licenses' subtitle='These are the licenses for the open source libraries used' contentLeft={darkMode.value ? <CertificationIconDark /> : <CertificationIconLight />}>
                         <Licenses brandColorStrict={brandColorStrict} />
